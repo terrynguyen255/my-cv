6d07846726913542009d7b3d03ff37256d9fa75d
Tu Nguyen <nogias9x@gmail.com>
2019-03-31 00:45:54 +0700

    migrate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,144 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {InfoField} from "./components/infoField/InforField";
+
+const me = {
+    commonInfo: {
+        dob: '14 - Nov - 1993',
+        email: 'nogias9x@gmail.com',
+        idCard: '079.193.004.392',
+        phone: '0378.678.349',
+        place: '458/17 Huynh Tan Phat Street, District 7, HCMC',
+        facebook: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+        github: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <div className="container main">
+              <div className="row section-light">
+                  <div className="col-3">
+                      <div className="avatar-container">
+                          <img
+                              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP2nkHA8F5_QHbZVpaLFZoHZPw_vAOxhBifk0C7BOcvxV3MaGpuw"/>
+                      </div>
+                  </div>
+                  <div className="col-9">
+                      <h1 className="name">TU NGUYEN</h1>
+                      <h1 className="title">BACKEND DEVELOPER</h1>
+
+                      <div className="row">
+                          {
+                              Object.keys(me.commonInfo).map(key => {
+                                  return (
+                                      <InfoField
+                                          type={key}
+                                          value={me.commonInfo[key]}
+                                      />
+                                  )
+                              })
+                          }
+                      </div>
+                  </div>
+              </div>
+
+
+              <div className="row section-dark">
+                  <div className="col-12">
+                      <div className="row">
+                          <div className="col-4">
+                              <div className="section-tag"><h3>MY TARGETS</h3></div>
+                          </div>
+                          <div className="col-8">
+                              Within next 2 years, I want to:
+                              <ul>
+                                  <li>Become a senior Website and Android developer</li>
+                                  <li>Use English fluently (100+ TOEFL)</li>
+                                  <li>Be able to lead a team of 4+ developers</li>
+                              </ul>
+                          </div>
+                      </div>
+
+                      <hr/>
+
+                      <div className="row">
+                          <div className="col-12">
+                              <div className="section-tag"><h3>MY SKILLS</h3></div>
+                          </div>
+                      </div>
+                      <div className="row">
+                          <div className="col-2">Skill name</div>
+                          <div className="col-4">
+                              <button className="point point-on"></button>
+                              <button className="point point-on"></button>
+                              <button className="point point-on"></button>
+                              <button className="point point-on"></button>
+                              <button className="point point-on"></button>
+                              <button className="point point-on"></button>
+                              <button className="point point-on"></button>
+                              <button className="point"></button>
+                              <button className="point"></button>
+                              <button className="point"></button>
+                          </div>
+                          <div className="col-6">description about the skill</div>
+                      </div>
+                      <div className="row">
+                          <div className="col-2">Skill name</div>
+                          <div className="col-4">points</div>
+                          <div className="col-6">description about the skill</div>
+                      </div>
+
+                  </div>
+              </div>
+
+
+              <div className="row section-light">
+                  <div className="col-12">
+                      <div className="row">
+                          <div className="col-6">
+                              <div className="section-tag"><h3>MY STRENGTHS</h3></div>
+                              <ul>
+                                  <li>High sense of responsibility</li>
+                                  <li>Hard-working</li>
+                                  <li>Good communication and team-working</li>
+                              </ul>
+                          </div>
+                          <div className="col-6">
+                              <ul>
+                                  <li>Following processes and coding-conventions consciously</li>
+                                  <li>Ability to work in a high pressure environment</li>
+                                  <li>Reading documents in English fairly</li>
+                              </ul>
+                          </div>
+                      </div>
+                      <hr/>
+                      <div className="row">
+                          <div className="col-6">
+                              <div className="section-tag"><h3>MY HOBBIES</h3></div>
+                              <ul>
+                                  <li>Reading books of literature, sciences and psychology</li>
+                              </ul>
+                          </div>
+                          <div className="col-6">
+                              <ul>
+                                  <li>Fishing</li>
+                                  <li>Making some handmade things</li>
+                                  <li>Fixing things</li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+
+              <div className="row">
+                  <div className="col-1">
+                      One of 1 columns
+                  </div>
+              </div>
+          </div>
     );
   }
 }
---
+++ src/components/infoField/InforField.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './InforField.css'
+
+export class InfoField extends Component {
+    render() {
+        const icon = _getIconPath(this.props.type)
+
+        return (
+            <div className="col-6">
+                <div className="info-field-icon-container">
+                    <img src={icon}/>
+                </div>
+                <div className="info-field-icon-value">{this.props.value}</div>
+            </div>
+        )
+    }
+}
+
+const _getIconPath = (type)=>{
+    if (type === 'dob') return `${process.env.PUBLIC_URL}/images/icon-dob.png`
+    if (type === 'email') return `${process.env.PUBLIC_URL}/images/icon-email.png`
+    if (type === 'idCard') return `${process.env.PUBLIC_URL}/images/icon-id-card.png`
+    if (type === 'phone') return `${process.env.PUBLIC_URL}/images/icon-phone.png`
+    if (type === 'place') return `${process.env.PUBLIC_URL}/images/icon-place.png`
+
+    if (type === 'facebook') return `${process.env.PUBLIC_URL}/images/icon-place.png`
+    if (type === 'github') return `${process.env.PUBLIC_URL}/images/icon-place.png`
+}
