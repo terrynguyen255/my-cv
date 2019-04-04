706b55f3dc54a93cd5bda329c7b22d33b028b7d5
Tu Nguyen <nogias9x@gmail.com>
2019-04-04 17:35:30 +0700

    add component Project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import {InfoField} from "./components/infoField/InforField";
 import {Skill} from "./components/skill/Skill";
 import {UnorderList} from "./components/unorderList/UnorderList";
+import {Project} from "./components/project/Project";
+import {ProjectStackType} from "./components/projectStack/ProjectStack";
+
+const moment = require('moment')
 
 const me = {
     commonInfo: {
@@ -60,6 +64,151 @@ const me = {
         'Fishing',
         'Making some handmade things',
         'Fixing things',
+    ],
+    projects: [
+        {
+            name: 'Predict It',
+            logo: `${process.env.PUBLIC_URL}/images/project-predictit-logo.png`,
+            owner: 'Sibyl Entertainment',
+            about: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec arcu suscipit, mattis leo nec, commodo odio. Phasellus imperdiet risus ut tristique hendrerit. Quisque elementum eu neque a rhoncus. Etiam eget nisi aliquet, pellentesque odio nec, imperdiet mauris. Phasellus sapien felis, volutpat non eros vel, consectetur tempor dui. In id dui a urna congue gravida. Morbi dignissim leo nec magna pulvinar hendrerit. Vivamus ultricies ultricies finibus. Etiam feugiat commodo aliquet. Nullam facilisis ut magna quis scelerisque. Proin laoreet nisi a massa volutpat, et condimentum nibh volutpat.`,
+            status: 'Beta',
+            stacks: [
+                ProjectStackType.PYTHON,
+                ProjectStackType.POSGRESQL,
+                ProjectStackType.CELERY,
+                ProjectStackType.DOCKER,
+                ProjectStackType.SWIFT,
+                ProjectStackType.KOTLIN,
+                ProjectStackType.REACT,
+                ProjectStackType.SLACK,
+                ProjectStackType.TRELLO,
+            ],
+            teamSize: 7,
+            myRoles: [{
+                name: 'Backend Dev',
+                description: ''
+            }],
+            from: moment('2019-1-18'),
+            to: moment('2019-3-21'),
+            photos: [
+                {
+                    title: '',
+                    subTitle: '',
+                    src: `${process.env.PUBLIC_URL}/images/project-predictit-photo-1.jpg`
+                }, {
+                    title: '',
+                    subTitle: '',
+                    src: `${process.env.PUBLIC_URL}/images/project-predictit-photo-2.jpg`
+                }, {
+                    title: '',
+                    subTitle: '',
+                    src: `${process.env.PUBLIC_URL}/images/project-predictit-photo-3.png`
+                }, {
+                    title: '',
+                    subTitle: '',
+                    src: `${process.env.PUBLIC_URL}/images/project-predictit-photo-4.jpg`
+                }, {
+                    title: '',
+                    subTitle: '',
+                    src: `${process.env.PUBLIC_URL}/images/project-predictit-photo-5.jpg`
+                },
+            ],
+        },
+        {
+            name: 'Home Ground',
+            logo: `${process.env.PUBLIC_URL}/images/project-rovo-logo.png`,
+            owner: 'Rovo',
+            about: `
+Home Ground is a new platform that targets Sports Businesses. Home Ground is initially targeted at clubs or sports halls that have physical facilities such as tennis courts, badminton courts, soccer pitches etc. The premise of Home Ground is to provide all the technology that a club needs to run their business effectively with just 10 minutes of setup. 
+While we’re starting with just sports clubs & halls, eventually Home Ground would even support advertisers, sports brands & shops. Home Ground will include public APIs that developers can integrate with to provide customised software or apps to Sports businesses.  For example, a smart lock device can integrate with APIs to allow unlocking of tennis courts from phones based on the bookings.
+Home Ground is also intended to be used by clubs worldwide. With the above factors in mind, it’s important to design Home Ground to support multi tenancy, localisation from the beginning. In addition to that, it’s important do design the architecture such that modules can be added incrementally by us internally & also be 3rd party developers in future.
+`,
+            status: 'Launching production',
+            stacks: [
+                ProjectStackType.NODEJS,
+                ProjectStackType.POSGRESQL,
+                ProjectStackType.KILLBILL,
+                ProjectStackType.BULL,
+                ProjectStackType.SWIFT,
+                ProjectStackType.KOTLIN,
+                ProjectStackType.REACT,
+                ProjectStackType.SLACK,
+                // ProjectStackType.BASECAMP,
+            ],
+            teamSize: 6,
+            myRoles: [
+                {
+                    name: 'Team leader',
+                    description: ''
+                }, {
+                    name: 'Backend developer',
+                    description: ''
+                }
+            ],
+            from: moment('2018-4-9'),
+            to: moment('2019-2-12'),
+            photos: [
+                {
+                    title: `Home screen`,
+                    subTitle: `All modules' shortcuts`,
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-1.png`
+                }, {
+                    title: 'Customers management',
+                    subTitle: 'Viewing, creating, updating, deleting, filtering, import customers from CSV',
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-2.png`
+                }, {
+                    title: 'Memberships management',
+                    subTitle: 'Viewing, creating, updating, deleting, filtering staff',
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-3.png`
+                }, {
+                    title: 'Ratings & reviews management',
+                    subTitle: `Reply customers' reviews, view statistics of ratings and reviews`,
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-4.png`
+                }, {
+                    title: 'Booking management',
+                    subTitle: 'Booking facility from user web',
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-5.png`
+                }, {
+                    title: 'Booking management',
+                    subTitle: 'Manage bookings from admin dashboard',
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-6.png`
+                }, {
+                    title: 'Staff management',
+                    subTitle: 'Viewing, creating, updating, deleting, filtering staff',
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-7.png`
+                }, {
+                    title: 'Email campaigns management',
+                    subTitle: 'Viewing, creating, updating, deleting, running, filtering campaigns',
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-8.png`
+                }, {
+                    title: 'Statistics',
+                    subTitle: 'Rendering statistics for pages views, revenues, reviews',
+                    src:`${process.env.PUBLIC_URL}/images/project-rovo-photo-9.png`
+                },
+            ],
+        },
+        // {
+        //     name: '',
+        //     logo: '',
+        //     owner: '',
+        //     about: '',
+        //     status: '',
+        //     stacks: [],
+        //     teamSize: '',
+        //     myPositions: [{
+        //         name: '',
+        //         description: ''
+        //     }],
+        //     from: new Date(),
+        //     to: new Date(),
+        //     photos: [
+        //         {
+        //             title: 'xxxx',
+        //             subTitle: 'xxxx',
+        //             src:`${process.env.PUBLIC_URL}/images/project-xxxxx-photo-xxxxxx.xxx`
+        //         },
+        //     ],
+        // }
     ]
 }
 
@@ -147,6 +296,16 @@ class App extends Component {
                     </div>
                 </div>
 
+                <div className="row section-dark">
+                    <div className="col-12">
+                        <div className="section-tag"><h3>MY PROJECTS</h3></div>
+                    </div>
+                    <div className="col-4">
+                        {
+                            me.projects.map(p => <Project project={p}/>)
+                        }
+                    </div>
+                </div>
 
                 <div className="row">
                     <div className="col-1">
---
+++ src/components/project/Project.js
@@ -0,0 +1,100 @@
+import React, {Component} from 'react'
+import './Project.css'
+import {
+    Modal,
+    ModalHeader,
+    ModalBody,
+} from 'reactstrap'
+import {ProjectStack} from "../projectStack/ProjectStack";
+import {PhotoCarousel} from "../PhotoCarousel/PhotoCarousel";
+
+const DATE_FORMAT = 'DD-MMM-YYYY'
+
+export class Project extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            detailOpened: false
+        }
+        this.toggleDetailDialog = this.toggleDetailDialog.bind(this)
+    }
+
+    render() {
+        const {
+            name,
+            logo,
+            about,
+            stacks,
+            teamSize,
+            myRoles,
+            from,
+            to,
+            photos,
+        } = this.props.project
+        const durationMonths = to.diff(from, 'months')
+
+        return (
+            <div
+                className='project-avatar'
+                onClick={this.toggleDetailDialog}
+            >
+                <div className='project-logo-container'><img src={logo}/></div>
+
+                <Modal
+                    isOpen={this.state.detailOpened}
+                    toggle={this.toggleDetailDialog}
+                    className='modal-lg'
+                >
+                    <ModalHeader toggle={this.toggleDetailDialog}>
+                        <div className='row'>
+                            <div className='col-2'>
+                                <div className='project-logo-container'><img src={logo}/></div>
+                            </div>
+                            <div className='col-10'>
+                                <p>{name}</p>
+                                <p>
+                                    <b>Duration:</b>&nbsp;
+                                    {durationMonths} months ({from.format(DATE_FORMAT)} - {to.format(DATE_FORMAT)})</p>
+                                <p>
+                                    <b>My role{myRoles.length > 1 ? 's' : ''}:</b>&nbsp;
+                                    { myRoles.map(role => role.name).join(', ') }
+                                </p>
+                            </div>
+                        </div>
+                    </ModalHeader>
+                    <ModalBody>
+                        <p><b>About:</b> {about}</p>
+                        <p><b>Team size:</b> {teamSize} members</p>
+                        <p><b>Stacks:</b></p>
+                        <div className='row' style={{margin:0}}>
+                            {
+                                stacks.map(stack => {
+                                    return (
+                                        <div className='col-1' style={{margin:0, padding:0}}>
+                                            <ProjectStack stack={stack}/>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <p><b>Photos:</b></p>
+                        <div className='row'>
+                            <div className='col-12'>
+                                <PhotoCarousel items={photos}/>
+                            </div>
+                        </div>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+
+    toggleDetailDialog() {
+        this.setState({
+            detailOpened: !this.state.detailOpened
+        })
+    }
+}
+
+Project.defaultProps = {
+}
