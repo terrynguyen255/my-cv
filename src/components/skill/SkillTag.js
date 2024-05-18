92f91153cba029f05a3024c2fc0cb611529dbf18
Terry Tu Nguyen <tu.nguyen@funnelbeam.com>
2024-05-18 18:43:47 +0700

    Hide skill scores

--- src/components/skill/SkillTag.js
+++ src/components/skill/SkillTag.js
@@ -7,15 +7,15 @@ import {getLocalizedValue} from "../../helpers/languageHelper";
 
 class SkillTag extends Component {
     render() {
-        const { i18n } = this.props;
+        const { i18n, showScores = false } = this.props;
         const name = getLocalizedValue(this.props.skill.name, i18n)
 
         return (
             <div className="skill-container">
                 <ReactTooltip id={name} type='info'>{`${this.props.skill.points}/10`}</ReactTooltip>
                 <div data-tip data-for={name}>
-                    {getLocalizedValue(name, i18n)}&nbsp;
-                    ({this.props.skill.points})
+                    {getLocalizedValue(name, i18n)}
+                    {showScores && <>(&nbsp;{this.props.skill.points})</>}
                 </div>
             </div>
         )
