8f1f9bb70a47c0e19dd6f5a58951a721f6837078
Terry Tu Nguyen <tu.nguyen@funnelbeam.com>
2024-05-19 15:14:48 +0700

    Fix time was parsed incorrectly

--- src/helpers/timeHelper.js
+++ src/helpers/timeHelper.js
@@ -1,5 +1,5 @@
 const moment = require('moment')
 
 export const mmoment = (dateStr) => {
-    return moment('2021-06-01', 'YYYY-MM-DD');
+    return dateStr ? moment(dateStr, 'YYYY-MM-DD') : moment();
 }
